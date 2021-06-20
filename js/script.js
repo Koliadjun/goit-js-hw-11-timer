b4d0b44189515fa28c7298dce438b93505f980a6
koliadjun <koliadjun@gmail.com>
2021-06-20 15:27:39 +0300

    timer is work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-import { days, hours, mins, secs } from "./date.js";
+// import { days, hours, mins, secs } from "./date.js";
+import CountdownTimer from "./timer.js"
 
 const timer = new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
+    targetDate: new Date('Jun 21, 2021'),
 });
+timer.startTimer()
--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,33 @@
-export const CountdownTimer = {
-    tr
+export default class CountdownTimer {
+    constructor({ selector, targetDate } = {}) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+        this.timerRefs = {
+            timer: document.querySelector(this.selector),
+            daysSpan: document.querySelector(`${this.selector} span[data-value="days"]`),
+            hoursSpan: document.querySelector(`${this.selector} span[data-value="hours"]`),
+            minsSpan: document.querySelector(`${this.selector} span[data-value="mins"]`),
+            secsSpan: document.querySelector(`${this.selector} span[data-value="secs"]`),
+        }
+
+    }
+    getTimerValue = () => {
+        let days = Math.floor((this.targetDate - Date.now()) / (1000 * 60 * 60 * 24));
+        let hours = Math.floor(((this.targetDate - Date.now()) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mins = Math.floor(((this.targetDate - Date.now()) % (1000 * 60 * 60)) / (1000 * 60));
+        let secs = Math.floor(((this.targetDate - Date.now()) % (1000 * 60)) / 1000);
+        return { days, hours, mins, secs }
+    }
+    renderValue = () => {
+        this.timerRefs.secsSpan.textContent = this.getTimerValue().secs;
+        this.timerRefs.minsSpan.textContent = this.getTimerValue().mins;
+        this.timerRefs.hoursSpan.textContent = this.getTimerValue().hours;
+        this.timerRefs.daysSpan.textContent = this.getTimerValue().days;
+    }
+
+
+
+    startTimer = () => {
+        setInterval(() => { this.renderValue() }, 1000)
+    }
 };
